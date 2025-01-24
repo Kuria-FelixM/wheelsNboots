dde8a988b7ce37b8bc61a7c95bb062e296c5e1ad
Kuria-FelixM <muhiafelix35@gmail.com>
2025-01-24 23:05:41 +0300

    quote

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // Toggle Sidebar Visibility
 function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
+    const sidebar = document.getElementById("sidenav");
     sidebar.classList.toggle("open");
   }
   
   // Hide Sidebar When Clicking Outside
   document.body.addEventListener("click", (e) => {
-    const sidebar = document.getElementById("sidebar");
+    const sidenav = document.getElementById("sidenav");
     const toggler = document.querySelector(".navbar-toggler");
   
     // Close sidebar if clicking outside and not on the toggle button
-    if (!sidebar.contains(e.target) && !toggler.contains(e.target)) {
-      sidebar.classList.remove("open");
+    if (!sidenav.contains(e.target) && !toggler.contains(e.target)) {
+      sidenav.classList.remove("open");
     }
   });
   
